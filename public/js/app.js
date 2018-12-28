5ff6b794502ffcc3d7089c3c9eb30e4d89438dae
smartboxit <smartbox.it@gmail.com>
2018-12-28 21:43:40 +0700

    update 7th

--- public/js/app.js
+++ public/js/app.js
@@ -1770,6 +1770,31 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      articles: [],
+      article: {
+        id: '',
+        title: '',
+        body: ''
+      },
+      article_id: '',
+      pagination: {},
+      edit: false
+    };
+  },
+  created: function created() {
+    this.fetchArticles();
+  },
+  methods: {
+    fetchArticles: function fetchArticles() {
+      fetch('api/articles').then(function (res) {
+        return res.json();
+      }).then(function (res) {
+        console.log(res.data);
+      });
+    }
+  },
   mounted: function mounted() {
     console.log('Component mounted.');
   }
@@ -48422,14 +48447,15 @@ if (token) {
 /*!**********************************************!*\
   !*** ./resources/js/components/Articles.vue ***!
   \**********************************************/
-/*! exports provided: default */
+/*! no static exports found */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Articles_vue_vue_type_template_id_03ba8128___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Articles.vue?vue&type=template&id=03ba8128& */ "./resources/js/components/Articles.vue?vue&type=template&id=03ba8128&");
 /* harmony import */ var _Articles_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Articles.vue?vue&type=script&lang=js& */ "./resources/js/components/Articles.vue?vue&type=script&lang=js&");
-/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _Articles_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _Articles_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
 
 
@@ -48459,7 +48485,7 @@ component.options.__file = "resources/js/components/Articles.vue"
 /*!***********************************************************************!*\
   !*** ./resources/js/components/Articles.vue?vue&type=script&lang=js& ***!
   \***********************************************************************/
-/*! exports provided: default */
+/*! no static exports found */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -48592,8 +48618,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\dev\git\test1003\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\dev\git\test1003\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! F:\project\Git\test1003\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! F:\project\Git\test1003\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/js/components/Articles.vue
+++ resources/js/components/Articles.vue
@@ -6,8 +6,36 @@
 
 <script>
     export default {
+
+        data() {
+            return {
+                articles:[],
+                    article: {
+                                id:'',
+                                title:'',
+                                body:''
+                            },
+                    article_id:'',
+                    pagination:{},
+                    edit:false
+                    }   
+                },
+        created() {
+                    this.fetchArticles();
+                     },
+        methods: {
+                    fetchArticles() {
+                        fetch('api/articles')
+                            .then(res=>res.json())
+                            .then(res=>{
+                                console.log(res.data);
+                            })
+                                    }
+                },
+        
         mounted() {
             console.log('Component mounted.')
+                   }
         }
-    }
+
 </script>
